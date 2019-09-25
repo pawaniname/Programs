764b401a0f858dd9238c83b7886957ba0a3042d3
pawaniname <37745925+pawaniname@users.noreply.github.com>
2019-09-25 01:32:02 -0700

    Add files via upload

---
+++ ReverseString.py
@@ -0,0 +1,5 @@
+#Program to take string input from user and print the revese of it
+
+str=input("Enter a String : ")   #To get input
+str2=str[::-1]                   #To copy reversed string to str2
+print(str2)                      #To print reversed string
